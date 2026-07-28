4ad0f4409c01380a9aab3ec8191bafa79ad9b3ac
Esrafil <esrafil.s.r.man418@gmail.com>
2026-07-28 16:22:52 +0330

    feat: Prefetch next page with TanStack Query

--- src/features/posts/hooks/usePosts.ts
+++ src/features/posts/hooks/usePosts.ts
@@ -1,11 +1,27 @@
 import { getPosts } from "@/features/posts/services/posts.service";
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import {
+  keepPreviousData,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { useEffect } from "react";
 
 export default function usePosts(search: string, page: number) {
-  return useQuery({
+  const queryClient = useQueryClient();
+
+  const query = useQuery({
     queryKey: ["posts", search, page],
     queryFn: () => getPosts(search, page),
 
     placeholderData: keepPreviousData,
   });
+
+  useEffect(() => {
+    queryClient.prefetchQuery({
+      queryKey: ["posts", search, page + 1],
+      queryFn: () => getPosts(search, page + 1),
+    });
+  }, [page, search, queryClient]);
+
+  return query;
 }
